e155fad7e861ccbbb026542babf1a9c5e3c9d7ad
tomazpecoraro <tomazpecoraro0@gmail.com>
2024-10-02 20:58:38 -0300

    crud

--- CrudApi/CrudApi.Tests/obj/Release/net8.0/CrudApi.Tests.AssemblyInfo.cs
+++ CrudApi/CrudApi.Tests/obj/Release/net8.0/CrudApi.Tests.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CrudApi")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CrudApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ceef64472cf5490d59028fceca859b740b2db1")]
-[assembly: System.Reflection.AssemblyProductAttribute("CrudApi")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CrudApi")]
+[assembly: System.Reflection.AssemblyProductAttribute("CrudApi.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CrudApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- CrudApi/Program.cs
+++ CrudApi/Program.cs
@@ -3,8 +3,6 @@ using CrudApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<MongoDbContext>();
@@ -14,7 +12,6 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
