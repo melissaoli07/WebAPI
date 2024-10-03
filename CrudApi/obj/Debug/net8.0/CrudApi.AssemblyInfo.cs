fde0cf6adbfc340911f4d1fbae2cc890a9ed9715
tomazpecoraro <tomazpecoraro0@gmail.com>
2024-10-02 21:02:55 -0300

    organizando

--- CrudApi/obj/Debug/net8.0/CrudApi.AssemblyInfo.cs
+++ CrudApi/obj/Debug/net8.0/CrudApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ceef64472cf5490d59028fceca859b740b2db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e155fad7e861ccbbb026542babf1a9c5e3c9d7ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
