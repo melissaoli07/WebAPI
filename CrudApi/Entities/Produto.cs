59b183607d2bf7af818d4f5a6026b21eed79ce6c
tomazpecoraro <tomazpecoraro0@gmail.com>
2024-10-02 22:54:05 -0300

    mongodb

--- CrudApi/Entities/Produto.cs
+++ CrudApi/Entities/Produto.cs
@@ -2,7 +2,7 @@ namespace CrudApi.Entities
 {
     public class Produto
     {
-        public string? Id { get; set; }
+        public int? Id { get; set; }
         public string? Nome { get; set; }
         public decimal Preco { get; set; }
     }
